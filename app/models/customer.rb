7d0df4bc1d5725fd35a764d51e1ac2ede276007e
raegertay <raegertay89@gmail.com>
2017-11-07 16:21:54 +0800

    Added facebook login

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,5 +2,17 @@ class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |customer|
+      customer.email = auth.info.email
+      customer.password = Devise.friendly_token[0,20]
+      # customer.name = auth.info.name   # assuming the customer model has a name
+      # customer.image = auth.info.image # assuming the customer model has an image
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # customer.skip_confirmation!
+    end
+  end
 end
