8eb8fbd7f3a2ebb16b3aedf6a52a989805dd1cfb
raegertay <raegertay89@gmail.com>
2017-11-08 13:55:36 +0800

    Set product index page as root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,15 @@ class ApplicationController < ActionController::Base
 
   private
 
-  # Overwriting the sign_out redirect path method
-  def after_sign_out_path_for(resource_or_scope)
-    if resource_or_scope == :customer
-      new_customer_session_path
-    elsif resource_or_scope == :admin
-      new_admin_session_path
-    else
-      root_path
-    end
-  end
+  # # Overwriting the sign_out redirect path method
+  # def after_sign_out_path_for(resource_or_scope)
+  #   if resource_or_scope == :customer
+  #     new_customer_session_path
+  #   elsif resource_or_scope == :admin
+  #     new_admin_session_path
+  #   else
+  #     root_path
+  #   end
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers, :controllers => { :omniauth_callbacks => "customers/omniauth_callbacks" }
 
+  root 'products#index'
+
   resources :products, only: [:index]
-  authenticated :customer do
-    root to: 'products#index', as: :authenticated_root
-  end
-  root to: redirect('/customers/sign_in')
 
   namespace :admin do
     resources :products, only: [:index]
