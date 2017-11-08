5bbcc6b8acb75e6de7f3449184f7802fd313d398
raegertay <raegertay89@gmail.com>
2017-11-08 10:48:35 +0800

    Added non-namespace routes, controllers & index view for products

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers, :controllers => { :omniauth_callbacks => "customers/omniauth_callbacks" }
 
+  resources :products
+
   namespace :admin do
     resources :products
   end
