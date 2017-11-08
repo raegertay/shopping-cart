808bf4380c8242eb9adda17d178bff9b4c7fe64b
raegertay <raegertay89@gmail.com>
2017-11-08 10:35:06 +0800

    Added namespace routes for admin/products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers, :controllers => { :omniauth_callbacks => "customers/omniauth_callbacks" }
+
+  namespace :admin do
+    resources :products
+  end
+
 end
