edd923a461881dece0f446f06bea4aec3bb1bc87
yspoo <pooyising@gmail.com>
2017-11-10 15:43:44 +0800

    Specs for Category model/controller done. Matched with Product model/controller specs.

--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -32,44 +32,45 @@ RSpec.describe Admin::CategoriesController, type: :controller do
       end
       it { is_expected.to render_template(:index) }
     end
+  end
 
-    describe 'GET #edit' do
-      let(:category) { create(:category) }
-      before { get :edit, params: ( id: category.id) }
-      it { expect(assigns(:category)).to eq(category) }
-    end
+  describe 'GET #edit' do
+    let(:category) { create(:category) }
+    before { get :edit, params: { id: category.id } }
+    it { expect(assigns(:category)).to eq(category) }
+  end
 
-    describe 'PUT/PATCH #update' do
-      let!(:category) { create(:category) }
-      before { put :update, params: params }
+  describe 'PUT/PATCH #update' do
+    let!(:category) { create(:category) }
+    before { put :update, params: params }
 
-      context 'when update is successful' do
-        let(:params) do
-          {
-            id: category.id,
-            brand: attributes_for(:category, name: "New Category name.")
-          }
-        end
-        it { is_expected.to redirect_to(admin_categories_path) }
+    context 'when update is successful' do
+      let(:params) do
+        {
+          id: category.id,
+          category: attributes_for(:category, name: "New Category name.")
+        }
       end
-
-      context 'when update is unsuccessful' do
-          let(:params) do
-            {
-              id: category.id,
-              category: attributes_for(:category, name: nil)
-            }
-          end
-          it { is_expected.to render_template(:edit) }
-      end
-
-    end
-
-    describe 'DELETE #destroy' do
-      let!(:brand) { create(:category) }
-      before { delete :destroy, params: { id: category.id} }
-      it { expect(assigns(:brand)).to be_destroyed }
       it { is_expected.to redirect_to(admin_categories_path) }
     end
 
+    context 'when update is unsuccessful' do
+      let(:params) do
+        {
+          id: category.id,
+          category: attributes_for(:category, name: nil)
+        }
+      end
+      it { is_expected.to render_template(:edit) }
+    end
+
   end
+
+  describe 'DELETE #destroy' do
+    let!(:category) { create(:category) }
+    before { delete :destroy, params: { id: category.id} }
+    it { expect(assigns(:category)).to be_destroyed }
+    it { is_expected.to redirect_to(admin_categories_path) }
+  end
+
+end
