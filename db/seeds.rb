291b5c7cdbcd3e725b436c5b65b76e906cbc35e1
gloria <gloriasoh553@hotmail.com>
2017-11-16 13:55:14 +0800

    Styled filter and product box

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,10 +13,18 @@ Brand.create(name: 'Tarte')
 Brand.create(name: 'Pixi')
 Brand.create(name: 'Sephora')
 
+Category.create(name: 'Face')
+Category.create(name: 'Lips')
+
 Product.create(name: 'Clay Mask', brand_id: 4, cost_price: 3 , selling_price: 11, stock: 10, images_attributes: [{url: 'https://cdn.filestackcontent.com/vSvMNomWQdufESEc2wr9', position: 1}], description: 'A line of face masks enriched with natural clay to address targeted skincare needs.')
+
 Product.create(name: 'Skintreat Duo', brand_id: 3, cost_price: 22 , selling_price: 59, stock: 10, images_attributes: [{url: 'https://cdn.filestackcontent.com/Yl6SjUPSSCbYMthRPGQP', position: 1}], description: 'A limited-edition duo of Pixi Beauty cult favourites for smoother, more luminous skin.' )
+
 Product.create(name: 'Everlasting Liquid Lipstick', brand_id: 1, cost_price: 15 , selling_price: 31, stock: 10, images_attributes: [{url: 'https://cdn.filestackcontent.com/kcv6I7JTFmdNHjacHCit', position: 1}], description: 'A long-wear, high-pigment liquid lipstick with a smooth, matte finish and all-day, lightweight comfort.' )
+
 Product.create(name: 'Lock-It Concealer Crème', brand_id: 1, cost_price: 19, selling_price: 40, stock: 10, images_attributes: [{url: 'https://cdn.filestackcontent.com/v6LlfC1DRiexX6l0SFzm', position: 1}], description: 'A super-versatile, rich and creamy concealer with 24-hour wear and instant full coverage.' )
 Product.create(name: 'Amazonian Finishing Powder', brand_id: 2, cost_price: 20, selling_price: 49, stock: 10, images_attributes: [{url: 'https://cdn.filestackcontent.com/NYfphF2R2GJRFKk1fFzV', position: 1}], description: 'This finishing powder keeps makeup in place without ever appearing chalky and mattifies skin by removing excess surface oil.' )
+
 Product.create(name: 'Amazonian Lipstick', brand_id: 2, cost_price: 8, selling_price: 26, stock: 10, images_attributes: [{url: 'https://cdn.filestackcontent.com/F7o9ZmS9aI1cVoVMAnVg', position: 1}], description: 'Each skin-flattering shade is enriched with sustainably harvested cupuacu and murumuru butters to deliver intense moisture and help restore and soothe even mature lips.' )
+
 Product.create(name: 'Rose Oil Blend', brand_id: 3, cost_price: 15, selling_price: 39, stock: 10, images_attributes: [{url: 'https://cdn.filestackcontent.com/1Tyu5JSvySUOqmjiYAvg', position: 1}], description: "This botanical blend is infused with a potent complex of youth-preserving oils to help improve skin's elasticity and glow. Sweet almond, rosehip, jojoba, pomegranate seed and rose geranium oils nourish the skin, leaving your complexion feeling comforted and smooth to the touch." )
