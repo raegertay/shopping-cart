26fb6befc2e2d7194095024fa07ae65993e4c200
gloria <gloriasoh553@hotmail.com>
2017-11-17 13:51:25 +0800

    Fixed navbar js

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,9 +1,19 @@
-window.onscroll = function() {scrollFunction()};
+$(document).on("turbolinks:load", function() {
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("fixed-navbar").style.top = "0";
+  var current_path = window.location.pathname;
+
+  if (current_path == '/' || current_path == '/home' || current_path == '/products' ) {
+      window.onscroll = function() {scrollFunction()};
   } else {
-    document.getElementById("fixed-navbar").style.top = "-67px";
+    document.getElementById("fixed-navbar").style.top = "0";
   }
-}
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("fixed-navbar").style.top = "0";
+    } else {
+      document.getElementById("fixed-navbar").style.top = "-67px";
+    }
+  }
+
+});
