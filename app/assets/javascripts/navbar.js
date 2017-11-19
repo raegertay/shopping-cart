df6268a4253a7029ed5ed23c8b4db729378d7fee
raegertay <raegertay89@gmail.com>
2017-11-19 12:52:20 +0800

    Bugfix for navbar not appearing sometimes

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -4,17 +4,23 @@ $(document).on("turbolinks:load", function() {
   var fixed_navbar = document.getElementById("fixed-navbar");
   if (fixed_navbar) {
     if (current_path == '/' || current_path == '/home' || current_path == '/products' ) {
+      // alert(current_path);
       window.onscroll = function() {scrollFunction()};
     } else {
+      window.onscroll = null;
+      // alert('Turbolink before: ' + document.getElementById("fixed-navbar").style.top);
       document.getElementById("fixed-navbar").style.top = "0";
+      // alert('Turbolink before: ' + document.getElementById("fixed-navbar").style.top);
     }
   }
 
   function scrollFunction() {
+    // alert('Running scrollfunction');
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
       document.getElementById("fixed-navbar").style.top = "0";
     } else {
       document.getElementById("fixed-navbar").style.top = "-67px";
+      // alert('Navbar hid');
     }
   }
 
